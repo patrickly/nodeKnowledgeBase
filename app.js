cb18d2f32230842022770d125eff65de4a6204e2
patrickly <patrickkimly@gmail.com>
2017-10-09 09:21:42 -0700

    Update Articles [Part 7 Start]

--- app.js
+++ app.js
@@ -72,6 +72,17 @@ app.get('/article/:id', function(req,res){
   });
 });
 
+// Load Edit Form
+app.get('/article/edit/:id', function(req,res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('edit_article', {
+      title:'Edit Article',
+      article: article
+    });
+  });
+});
+
+
 // Add Route
 app.get('/articles/add', function(req, res){
   res.render('add_articles', {
@@ -87,6 +98,25 @@ app.post('/articles/add', function(req, res){
   article.body =  req.body.body;
 
   article.save(function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else{
+      res.redirect('/');
+    }
+  })
+});
+
+// Update Submit POST route
+app.post('/articles/edit/:id', function(req, res){
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body =  req.body.body;
+
+  let query = {_id:req.params.id}
+
+  Article.update(query, article, function(err){
     if(err){
       console.log(err);
       return;
