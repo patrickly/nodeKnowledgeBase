e524991aaaab25a180ba6bb8d570ac3e9b57ff46
patrickly <patrickkimly@gmail.com>
2017-10-09 06:27:22 -0700

    middleware and app.post on adding articles [Part 5 End]

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const dbConnectionOptions = {
   useMongoClient: true,
@@ -37,6 +38,13 @@ const app = express();
  app.set('views', path.join(__dirname, 'views'));
  app.set('view engine', 'pug');
 
+ // Body parser middleware
+ // parse application/x-www-form-urlencoded
+ app.use(bodyParser.urlencoded({ extended: false }))
+
+ // parse application/json
+ app.use(bodyParser.json())
+
 // Home Route
 app.get('/', function(req, res){
   Article.find({}, function(err, articles){
@@ -63,8 +71,18 @@ app.get('/articles/add', function(req, res){
 // Add Submit POST route
 app.post('/articles/add', function(req, res){
   let article = new Article();
-//  article.title = req.body.title;
-  console.log(req.body.title);
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body =  req.body.body;
+
+  article.save(function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else{
+      res.redirect('/');
+    }
+  })
 });
 
 
