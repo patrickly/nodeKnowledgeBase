cd19cd28c583c7b76828299efa1f5cb913e1e423
patrickly <patrickkimly@gmail.com>
2017-10-09 17:58:59 -0700

    [Part 9 End] Login and Register

--- app.js
+++ app.js
@@ -103,7 +103,11 @@ app.get('/', function(req, res){
 
 
 let articles = require('./routes/articles');
+let users = require('./routes/users');
 app.use('/articles', articles);
+app.use('/users', users);
+
+
 
 // Start Server
 app.listen(3000, function(){
